90f79dba0ea940d024f1585fb4b72dedd1313d87
Alexander W Greene <greene.al1991@gmail.com>
2017-06-01 10:07:23 +0000

    Fix  call and clean up index page

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
-    methodOverride = require("method-override");
-    expressSanatizer = require("express-sanitizer");
+var bodyParser = require("body-parser"),
+    methodOverride = require("method-override"),
+    expressSanatizer = require("express-sanitizer"),
+    mongoose = require("mongoose"),
+    express = require("express"),
+    app = express();
  
 // APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(expressSanatizer);
+app.use(expressSanatizer());
 app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
